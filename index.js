721c3b8d3f4da393047b7734f682ecfb398f4f81
Force <forcesalmones@gmail.com>
2022-09-26 18:44:09 -0400

    Serve a form with a textbox and button

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 (0, http_1.createServer)(function (req, res) {
-    res.write('Hello World');
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<form>
+        <input type="text" id="textBox">
+        <button type="button">Hello!</button>
+        </form>`);
     res.end();
 }).listen(3000);
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import {createServer, IncomingMessage, ServerResponse} from 'http';
 
 createServer(function (req:IncomingMessage, res:ServerResponse) {
-    res.write('Hello World');
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+        `<form>
+        <input type="text" id="textBox">
+        <button type="button">Hello!</button>
+        </form>`);
     res.end();
 }).listen(3000);
