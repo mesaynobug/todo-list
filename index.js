94803199ddf13c829e33beb890113ec99c320b69
Force <forcesalmones@gmail.com>
2022-09-26 23:32:14 -0400

    Make form send a POST request on button click

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 (0, http_1.createServer)(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`<form>
-        <input type="text" id="textBox">
-        <button type="button">Hello!</button>
-        </form>`);
+    res.write(`<form action="" method="post">
+            <input type="text" id="textBox" name=textBox>
+            <button>Hello!</button>
+            </form>`);
+    if (req.method == "POST") {
+        let data = "";
+        req.on("data", (chunk) => {
+            data += chunk;
+        });
+        req.on("end", () => {
+            let usp = new URLSearchParams(data);
+            console.log(usp.get("textBox"));
+        });
+    }
     res.end();
 }).listen(3000);
--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
-import {createServer, IncomingMessage, ServerResponse} from 'http';
+import { createServer, IncomingMessage, ServerResponse } from "http";
 
-createServer(function (req:IncomingMessage, res:ServerResponse) {
+createServer(function (req: IncomingMessage, res: ServerResponse) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(
-        `<form>
-        <input type="text" id="textBox">
-        <button type="button">Hello!</button>
-        </form>`);
+      `<form action="" method="post">
+            <input type="text" id="textBox" name=textBox>
+            <button>Hello!</button>
+            </form>`
+    );
+
+    if (req.method == "POST") {
+      let data: string = "";
+      req.on("data", (chunk) => {
+        data += chunk;
+      });
+      req.on("end", () => {
+        let usp: URLSearchParams = new URLSearchParams(data);
+        console.log(usp.get("textBox"));
+      });
+    }
     res.end();
 }).listen(3000);
