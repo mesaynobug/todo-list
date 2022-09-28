26496de9e4add1250fa757f68de4bd5759b28e3b
Force <forcesalmones@gmail.com>
2022-09-28 16:50:39 -0400

    Serve form with array contents

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 let tasks = [];
+let tasksStr = "";
+let myForm = `<form action="" method="post">
+                      <input type="text" id="textBox" name=textBox autofocus="autofocus">
+                      <button type="submit">Hello!</button>
+                      </form>`;
 (0, http_1.createServer)(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`<form action="" method="post">
-            <input type="text" id="textBox" name=textBox>
-            <button>Hello!</button>
-            </form>`);
+    res.write(myForm);
+    tasksStr = "";
     if (req.method == "POST") {
         let data = "";
         req.on("data", (chunk) => {
@@ -15,11 +18,21 @@ let tasks = [];
         });
         req.on("end", () => {
             let usp = new URLSearchParams(data);
-            tasks.push(usp.get("textBox"));
+            if (usp.get("textBox") != "") {
+                tasks.push(usp.get("textBox"));
+            }
+            for (let element in tasks) {
+                tasksStr += tasks[element] + "<br>";
+            }
+            res.write(tasksStr);
+            res.end();
         });
     }
-    for (let element in tasks) {
-        console.log(tasks[element]);
+    else {
+        for (let element in tasks) {
+            tasksStr += tasks[element] + "<br>";
+        }
+        res.write(tasksStr);
+        res.end();
     }
-    res.end();
 }).listen(3000);
--- index.ts
+++ index.ts
@@ -1,24 +1,35 @@
 import { createServer, IncomingMessage, ServerResponse } from "http";
 let tasks: String[] = [];
+let tasksStr: string = "";
+let myForm: string = `<form action="" method="post">
+                      <input type="text" id="textBox" name=textBox autofocus="autofocus">
+                      <button type="submit">Hello!</button>
+                      </form>`;
 createServer(function (req: IncomingMessage, res: ServerResponse) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(
-    `<form action="" method="post">
-            <input type="text" id="textBox" name=textBox>
-            <button>Hello!</button>
-            </form>`
-  );
-
-  if (req.method == "POST") {
-    let data: string = "";
-    req.on("data", (chunk) => {
-      data += chunk;
-    });
-    req.on("end", () => {
-    let usp: URLSearchParams = new URLSearchParams(data);
-        tasks.push(usp.get("textBox")!);
-    });
-  }
-  for (let element in tasks){console.log(tasks[element]);}
-  res.end();
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(myForm);
+    tasksStr = "";
+    if (req.method == "POST") {
+        let data: string = "";
+        req.on("data", (chunk) => {
+            data += chunk;
+        });
+        req.on("end", () => {
+            let usp: URLSearchParams = new URLSearchParams(data);
+            if (usp.get("textBox") != "") {
+                tasks.push(usp.get("textBox")!);
+            }
+            for (let element in tasks) {
+                tasksStr += tasks[element] + "<br>";
+            }
+            res.write(tasksStr);
+            res.end();
+        });
+    }
+    else{
+        for (let element in tasks) {
+            tasksStr += tasks[element] + "<br>";
+        }
+        res.write(tasksStr);
+        res.end();}
 }).listen(3000);
