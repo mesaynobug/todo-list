6d1798629022145bffe35dae1f529473d73e2510
Force <forcesalmones@gmail.com>
2023-03-12 16:47:26 -0400

    Implement filtering tasks by tags

--- src/ListCommand.ts
+++ src/ListCommand.ts
@@ -9,21 +9,39 @@ export class ListCommand implements Command {
     static readonly COMMAND_WORD: string = "list";
     async run(input: string, res: ServerResponse, db: Database): Promise<void> {
         let tasksStr = "";
-        const archive: boolean = input === "ARCHIVE";
+        const args: string[] = input.split(" ");
+        const archive: boolean = args[0] === "ARCHIVE";
+        const tag: boolean = args[0] === "TAG";
         const idArr: number[] = await db.list();
         const taskArr: Task[] = await Promise.all(
             idArr.map((id) => db.read(id))
         );
-        taskArr.map((task) => {
-            if (
-                ((task.getDesc().search(input) !== -1 || input.trim() === "") &&
-                    !archive &&
-                    !task.getArchived()) ||
-                (archive && task.getArchived())
-            ) {
-                tasksStr += task.toString();
-            }
-        });
+
+        if (archive) {
+            taskArr.map((task) => {
+                if (task.getArchived()) {
+                    tasksStr += task.toString();
+                }
+            });
+        }
+        if (tag) {
+            taskArr.map((task) => {
+                if (task.getTags().find((e) => e == args[1])) {
+                    tasksStr += task.toString();
+                }
+            });
+        }
+        if (!tag && !archive) {
+            taskArr.map((task) => {
+                if (
+                    (task.getDesc().search(input) !== -1 ||
+                        input.trim() === "") &&
+                    !task.getArchived()
+                ) {
+                    tasksStr += task.toString();
+                }
+            });
+        }
         res.write(tasksStr);
         res.end();
     }
